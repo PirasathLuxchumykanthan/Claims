10ab6287894252d9338446f85de403bd35a373b7
Pirasath <connect@object.social>
2022-10-03 23:29:26 +0200

    mesage if you can read this. then u pretty cool

--- Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
+++ Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
@@ -3,6 +3,7 @@ public class ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail
 {
     /// <summary>
     /// I made this software beaucse i like github and this time system 
+    /// pretty cool so when i make change there are a history what i do
     /// </summary>
    public Software Software { get; init; }
 }
