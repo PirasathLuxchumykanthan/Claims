e36dffab5981b40dfe5e6d7de2756b560968eed0
Pirasath <connect@object.social>
2022-10-03 23:27:15 +0200

    Just a reminder

--- Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
+++ Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
@@ -1,5 +1,8 @@
 ﻿namespace Claims;
 public class ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail
 {
+    /// <summary>
+    /// I made this software beaucse i like github and this time system 
+    /// </summary>
    public Software Software { get; init; }
 }
