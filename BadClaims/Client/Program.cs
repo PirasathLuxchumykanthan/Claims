b51ee2c727b6fc460fbb1f92455683d84f286af5
Pirasath <connect@object.social>
2022-10-03 23:55:34 +0200

    btw i made this public to tell my story

--- BadClaims/Client/Program.cs
+++ BadClaims/Client/Program.cs
@@ -9,5 +9,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 //btw all comment her also log.. in this story behind the code.
 //as i thing need to be social..
 //so wild u got it it every comment i made a story to
+//btw i made this public to tell my story
 builder.Services.AddScoped(x => new Claims.ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail() { Software=Claims.Software.Private });
 await builder.Build().RunAsync();
