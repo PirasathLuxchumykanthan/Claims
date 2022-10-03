069ee5b9a7b7434915df05e02b5054eec3154a63
Pirasath <connect@object.social>
2022-10-04 00:09:03 +0200

    btw if you understnd this... then is cool.. live chat to...follow how like to follow see what the did.. try read them

--- MemoryClaims/Client/Program.cs
+++ MemoryClaims/Client/Program.cs
@@ -6,5 +6,6 @@ builder.RootComponents.Add<App>("main");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //btw i feel this book need to be free and live.. so if you understand it then 3 level sick story 
+//no big deal
 builder.Services.AddScoped(x => new Claims.ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail() { Software = Claims.Software.Private });
 await builder.Build().RunAsync();
