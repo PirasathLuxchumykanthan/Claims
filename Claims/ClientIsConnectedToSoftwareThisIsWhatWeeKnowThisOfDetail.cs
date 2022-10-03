69c9ff66a2cb5a2db120d9c892216c13741fb6db
Pirasath <connect@object.social>
2022-10-04 00:31:10 +0200

    explain the story in behind code in comment

--- Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
+++ Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
@@ -16,5 +16,10 @@ public class ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail
     /// what i do.. can be a story from a text... follow me..
     /// okay btw if you developer thats funny i made 2 story..
     /// </summary>
-   public Software Software { get; init; }
+    public Software Software { get; init; }
+    //i made  it like this the software claims are the main the social..
+    //but i need a business to.. so i made i like this to..
+    //made it default bce wee have more claims project then "OS" OBJECT:SOCIAL
+    //you see the story is by blink  you can read this.. and after i can remove it
+    public Service Service { get; init; } = Service.Claims;
 }
