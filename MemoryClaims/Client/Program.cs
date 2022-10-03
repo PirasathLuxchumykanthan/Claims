bf2f9dcc39c9d5e8745b85b7b53d92f0e9138864
Pirasath <connect@object.social>
2022-10-04 00:06:39 +0200

    i need help and i need it i will make it by my self .. and hope you will join this,.. make a live event of wee do in this..

--- MemoryClaims/Client/Program.cs
+++ MemoryClaims/Client/Program.cs
@@ -5,4 +5,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("main");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//btw i feel this book need to be free and live.. so if you understand it then 3 level sick story
+builder.Services.AddScoped(x => new Claims.ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail() { Software = Claims.Software.Private });
 await builder.Build().RunAsync();
