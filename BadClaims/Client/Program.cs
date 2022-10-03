d57908165fcb61ac9a032bfbd9c0fc3559266f37
Pirasath <connect@object.social>
2022-10-04 00:00:34 +0200

    this is my art

--- BadClaims/Client/Program.cs
+++ BadClaims/Client/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 //so wild u got it it every comment i made a story to
 //btw i made this public to tell my story
 //its so sick i make a other level of comnication sorry i dont spell well haha but u can make fun of it
-//lets connect every one in this world and show them how to connect
+//lets connect every one in this world and show them how to connect thats why connecting togher 
+//this story is my me.. thx for reading the comment.,
+//my comment is my story in this book and this is me.. and my art... crazy art.. but my art
 builder.Services.AddScoped(x => new Claims.ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail() { Software=Claims.Software.Private });
 await builder.Build().RunAsync();
