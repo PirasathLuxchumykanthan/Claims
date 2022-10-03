905cd5531e86dc6d129550382a6eef94724ea46d
Pirasath <connect@object.social>
2022-10-03 23:33:17 +0200

    thats sick... i will make a story of my project.. and if you what to be parth of this.... join me.. this is sick... make something and put it out see if ppl arround this world can see my story

--- Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
+++ Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
@@ -6,6 +6,14 @@ public class ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail
     /// pretty cool so when i make change there are a history what i do
     /// are time story what i do
     /// i can make real story a fuckt up story nobody can understand it
+    /// what and also story in the text here thats sick... made by github...
+    /// 
+    /// MY STORY Pirasath Luxchumykantahen this is my real story..
+    /// and its sick..
+    /// 
+    /// i dream about to make a story like this.. time by time..
+    /// 
+    /// what i do.. can be a story from a text... follow me..
     /// </summary>
    public Software Software { get; init; }
 }
