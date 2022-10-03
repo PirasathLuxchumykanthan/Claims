25a8b95a74aff828ccbd59830d0e30485e30c90d
Pirasath <connect@object.social>
2022-10-03 23:30:46 +0200

    I story if you can read this... then you really sick

--- Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
+++ Claims/ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail.cs
@@ -5,6 +5,7 @@ public class ClientIsConnectedToSoftwareThisIsWhatWeeKnowThisOfDetail
     /// I made this software beaucse i like github and this time system 
     /// pretty cool so when i make change there are a history what i do
     /// are time story what i do
+    /// i can make real story a fuckt up story nobody can understand it
     /// </summary>
    public Software Software { get; init; }
 }
