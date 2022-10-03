b67e0cfea1cfd15c405edd4d8320616bed684e54
Pirasath <connect@object.social>
2022-10-04 01:23:43 +0200

    Make it a way to new stuff of devices

--- Claims/Entrance.cs
+++ Claims/Entrance.cs
@@ -8,6 +8,17 @@ namespace Claims
 {
     public enum Entrance
     {
-        Firefox
+        //wee need to know some software i made something cool before let me add it
+        //i let other browser connect to Maybe wee can make software for whact or someting more cool that time what will come
+        Unknown = 0,
+        Android = 1,
+        Mac = 2,
+        Microsoft = 3,
+        iOS = 4,
+        Firefox = 5,
+        Oprea = 6,
+        Edge = 7,
+        Chrome = 8,
+        Safari = 9
     }
 }
